a9e0e37e2fe6a28ef30ee1f3bfbc53112fa3aed1
Scott Powers <saberpowers@gmail.com>
2024-12-19 17:45:09 -0600

    improve documentation for game_type

--- R/sanitize_game_type.R
+++ R/sanitize_game_type.R
@@ -2,23 +2,23 @@
 #' 
 #' Check that a character vector of game types are all valid game types.
 #' 
-#' @param game_type character vector of game types
+#' @param game_type character vector of game types. Valid game types are:
+#' \itemize{
+#'   \item{"R" for Regular Season}
+#'   \item{"F" for First-Round Playoff Series (aka wild card)}
+#'   \item{"D" for Division Series (or minor-league quarter-finals)}
+#'   \item{"L" for League Championship Series (or minor league semi-finals)}
+#'   \item{"W" for World Series (or minor league championship)}
+#'   \item{"A" for All-Star Game}
+#'   \item{"E" for Exhibition}
+#' }
 #' 
 #' @return simply returns the input vector `game_type` but throws error if invalid types are present
 #' 
 sanitize_game_type <- function(game_type) {
   game_type <- match.arg(
     arg = game_type,
-    choices = c(
-      "R",  # regular season
-      "F",  # first-round playoff series, aka wild card
-      "D",  # division series
-      "L",  # league championship series
-      "W",  # world series
-      "S",  # spring training
-      "A",  # all-star game
-      "E"   # exhibition
-    ),
+    choices = c("R", "F", "D", "L", "W", "S", "A", "E"),
     several.ok = TRUE
   )
   return(game_type)
