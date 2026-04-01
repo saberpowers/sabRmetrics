e86d90c9e54f96e660d05bd4cfbdf5d77dccc5f8
Scott Powers <saberpowers@gmail.com>
2026-03-31 21:33:22 -0500

    Extract ABS challenge data from MLB Stats API (#34)
    * add challenge/review data to extract_game()
    
    * increment package version number and update changelog
    
    * add test for identifying ABS challenges

--- tests/testthat/test-download_statsapi.R
+++ tests/testthat/test-download_statsapi.R
@@ -1,3 +1,11 @@
 test_that("statsapi one-day download works", {
   expect_no_error(download_statsapi(start_date = "2024-07-01", end_date = "2024-07-01"))
 })
+
+test_that("March 30, 2026, has 44 challenges", {
+  march_30_2026 <- download_statsapi(start_date = '2026-03-30', end_date = '2026-03-30')
+  n_challenges <- march_30_2026$pitch |>
+    dplyr::filter(abs_challenged) |>
+    nrow()
+  expect_equal(n_challenges, 44)
+})
