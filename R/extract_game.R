saberpowers/sabRmetrics

commit e86d90c9e54f96e660d05bd4cfbdf5d77dccc5f8

Extract ABS challenge data from MLB Stats API (#34)
* add challenge/review data to extract_game()

* increment package version number and update changelog

* add test for identifying ABS challenges

R/extract_game.R
@@ -63,6 +63,11 @@ extract_game <- function(game_id) {
     from_catcher = replace_null(play_data$details$fromCatcher),
     runner_going = replace_null(play_data$details$runnerGoing),
     is_out = play_data$details$isOut,
+    is_reviewed = play_data$details$hasReview,
+    is_overturned = replace_null(play_data$reviewDetails$isOverturned),
+    review_type = replace_null(play_data$reviewDetails$reviewType),
+    challenge_team_id = replace_null(play_data$reviewDetails$challengeTeamId),
+    challenge_player_id = replace_null(play_data$reviewDetails$player$id),
     pitch_type = play_data$details$type$code,
     ax = play_data$pitchData$coordinates$aX,
     ay = play_data$pitchData$coordinates$aY,
@@ -94,6 +99,14 @@ extract_game <- function(game_id) {
     pre_balls = dplyr::coalesce(dplyr::lag(post_balls, 1), 0),
     pre_strikes = dplyr::coalesce(dplyr::lag(post_strikes, 1), 0),
     pre_disengagements = dplyr::coalesce(dplyr::lag(post_disengagements, 1), 0),
+    # I haven't figured out the pattern yet, but as far as I can tell, ...
+    #   - "MA" corresponds to challenging a tag play
+    #   - "MJ" corresponds to ABS challenge
+    #   - "NH" corresponds to challenging a fair/foul call
+    abs_challenged = is_reviewed & (review_type == "MJ"),
+    abs_overturned = ifelse(abs_challenged, is_overturned, NA),
+    abs_team_id = ifelse(abs_challenged, challenge_team_id, NA),
+    abs_player_id = ifelse(abs_challenged, challenge_player_id, NA)
   ) |>
   dplyr::ungroup()
 
@@ -101,6 +114,7 @@ extract_game <- function(game_id) {
     dplyr::filter(type == "pitch") |>
     dplyr::select(play_id, game_id, event_index, play_index, pitch_number,
       outs, balls = pre_balls, strikes = pre_strikes,
+      abs_challenged, abs_overturned, abs_team_id, abs_player_id,
       description, pitch_type, ax, ay, az, vx0, vy0, vz0, x0, z0, extension, spin_rate,
       strike_zone_top, strike_zone_bottom, launch_speed, launch_angle, hit_coord_x, hit_coord_y
     )
@@ -177,11 +191,10 @@ extract_game <- function(game_id) {
     ) |>
     dplyr::select(play_id, game_id, event_index, play_index, pitch_number,
       pre_runner_1b_id, pre_runner_2b_id, pre_runner_3b_id, pre_outs, pre_balls, pre_strikes,
-      pre_disengagements,
-      runs_on_play,
       post_runner_1b_id, post_runner_2b_id, post_runner_3b_id, post_outs, post_balls, post_strikes,
       post_disengagements, type, runner_going, from_catcher,
-      is_pickoff, is_pickoff_error, is_stolen_base, is_caught_stealing, is_defensive_indiff
+      is_pickoff, is_pickoff_error, is_stolen_base, is_caught_stealing, is_defensive_indiff,
+      is_reviewed, is_overturned, review_type, challenge_team_id, challenge_player_id
     )
 
 

DESCRIPTION
@@ -1,6 +1,6 @@
 Package: sabRmetrics
 Title: Query {statsapi,baseballsavant}.mlb.com and fit fundamental sabermetric models
-Version: 1.2.0
+Version: 1.2.1
 Authors@R: 
     person("Scott", "Powers", email = "scott.powers@rice.edu", role = c("aut", "cre"))
 Description: The highest-fidelity raw MLB data are available from statsapi.mlb.com and baseballsavant.mlb.com, and some minor league data are available from these APIs as well. This package provides functions to query those APIs, returning the data in a collection of well-factored tables (as opposed to returning a single table). Additionally, this package offers functions to estimate base-out run expectancy; estimate count values; and recover the full estimated quadratic trajectory of a pitch from pitch tracking data.

tests/testthat/test-download_statsapi.R
@@ -1,3 +1,11 @@
 test_that("statsapi one-day download works", {
   expect_no_error(download_statsapi(start_date = "2024-07-01", end_date = "2024-07-01"))
 })
+
+test_that("March 30, 2026, has 44 challenges", {
+  march_30_2026 <- download_statsapi(start_date = '2026-03-30', end_date = '2026-03-30')
+  n_challenges <- march_30_2026$pitch |>
+    dplyr::filter(abs_challenged) |>
+    nrow()
+  expect_equal(n_challenges, 44)
+})

commit b1a355b07d4d6ae7129d73ed2b7b44ee8a5fbd4a

fix bug introduced by #34 in extract_game()

R/extract_game.R
@@ -191,6 +191,7 @@ extract_game <- function(game_id) {
     ) |>
     dplyr::select(play_id, game_id, event_index, play_index, pitch_number,
       pre_runner_1b_id, pre_runner_2b_id, pre_runner_3b_id, pre_outs, pre_balls, pre_strikes,
+      pre_disengagements, runs_on_play,
       post_runner_1b_id, post_runner_2b_id, post_runner_3b_id, post_outs, post_balls, post_strikes,
       post_disengagements, type, runner_going, from_catcher,
       is_pickoff, is_pickoff_error, is_stolen_base, is_caught_stealing, is_defensive_indiff,